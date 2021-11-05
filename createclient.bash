786cbde55df281a1d86df8cf57cf2cde26572dc1
TGR <78166079+TGACPNV@users.noreply.github.com>
2021-11-05 13:32:45 +0100

    replace echo by printf

--- createclient.bash
+++ createclient.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$EUID" -ne 0 ]
-  then echo "Error: Please run as root. Aborted."
+  then printf "Error: Please run as root. Aborted."
   exit
 fi
 
@@ -12,18 +12,18 @@ passwordok=0
 
 
 while [[ $clientok -eq 0 ]]; do
-    echo -n "Username: "
+    printf "Username: "
     read -r client
     if [[ -n $client ]]; then
             clientok=1
     else
-            echo "\nPlease enter a username!\n"
+            printf "\nPlease enter a username!\n"
     fi
 done
 
 while [[ $passwordok -eq 0 ]]; do
     # from https://stackoverflow.com/questions/2654009/how-to-make-bash-script-ask-for-a-password
-    echo -n "Password: "
+    printf "Password: "
     stty_orig=$(stty -g) # save original terminal setting.
     stty -echo           # turn-off echoing.
     read -r password  # read the password
@@ -31,13 +31,13 @@ while [[ $passwordok -eq 0 ]]; do
 
     if [[ -n $password ]]; then
             passwordok=1
-        else echo "\nPlease enter a password!\n"
+        else printf "\nPlease enter a password!\n"
     fi
 done
 
 
 # create user
-echo "\nCreating user"
+printf "\nCreating user"
 
 useradd ${client} -p $(openssl passwd -crypt ${password}) -m -d "/home/${client}" -s /bin/bash
 
@@ -53,7 +53,7 @@ echo "umask 007" >> /home/${client}/.bashrc
 
 # create nginx config
 
-echo "\ncreating nginx config"
+printf "\ncreating nginx config"
 
 echo "server {
     listen 80;
@@ -80,7 +80,7 @@ ln -s /etc/nginx/sites-available/${client} /etc/nginx/sites-enabled/
 
 # create php config
 
-echo "\ncreating php config"
+printf "\ncreating php config"
 
 echo "[${client}]
 user = ${client}
@@ -100,7 +100,7 @@ systemctl restart nginx
 systemctl restart php7.4-fpm
 
 #database
-echo "\nCreating mariadb database"
+printf "\nCreating mariadb database"
 
 query_mysql="CREATE DATABASE ${client};
 GRANT ALL PRIVILEGES ON ${client}.* TO '${client}'@'%' IDENTIFIED BY '${password}';
