35888fe3a0c4c4b60ff42a95477734becc4b8d02
TGR <78166079+TGACPNV@users.noreply.github.com>
2021-11-05 13:09:57 +0100

    bug fixes

--- createclient.bash
+++ createclient.bash
@@ -1,57 +1,59 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ "$EUID" -ne 0 ]
   then echo "Error: Please run as root. Aborted."
   exit
 fi
 
-clientok="false"
-passwordok="false"
 
-while [$clientok -eq "flase"]
-do
-    echo "Username: "
-    IFS= read -r client
-    if [-n $client]
-        then $clientok = "true"
-        else echo "Please enter a username!\n"
+clientok=0
+passwordok=0
+
+
+
+while [[ $clientok -eq 0 ]]; do
+    echo -n "Username: "
+    read -r client
+    if [[ -n $client ]]; then
+            clientok=1
+    else
+            echo "\nPlease enter a username!\n"
     fi
 done
 
-while [$passwordok -eq "false"]
-do
+while [[ $passwordok -eq 0 ]]; do
     # from https://stackoverflow.com/questions/2654009/how-to-make-bash-script-ask-for-a-password
-    echo "Password: "
+    echo -n "Password: "
     stty_orig=$(stty -g) # save original terminal setting.
     stty -echo           # turn-off echoing.
-    IFS= read -r password  # read the password
+    read -r password  # read the password
     stty "$stty_orig"    # restore terminal setting.
 
-    if [-n $password]
-        then $passwordok = "true"
-        else echo "Please enter a password!\n"
+    if [[ -n $password ]]; then
+            passwordok=1
+        else echo "\nPlease enter a password!\n"
     fi
 done
 
 
 # create user
-echo "Creating user"
+echo "\nCreating user"
 
-useradd ${client} -p $(openssl passwd -crypt ${password}) -m -d /home/${client} -s /bin/bash
+useradd ${client} -p $(openssl passwd -crypt ${password}) -m -d "/home/${client}" -s /bin/bash
 
 mkdir /home/${client}/www
 
-touch /home/${client}/www/index.php "${client}" 
+touch /home/${client}/www/index.php ${client}
 
-chown ${client}:www-data -R /home/${client}
+chown ${client}:${client} -R /home/${client}
 
 chmod 770 -R /home/${client}
 
-echo "umask 007" >> sudo /home/${client}/.bashrc
+echo "umask 007" >> /home/${client}/.bashrc
 
 # create nginx config
 
-echo "creating nginx config"
+echo "\ncreating nginx config"
 
 echo "server {
     listen 80;
@@ -78,7 +80,7 @@ ln -s /etc/nginx/sites-available/${client} /etc/nginx/sites-enabled/
 
 # create php config
 
-echo "creating php config"
+echo "\ncreating php config"
 
 echo "[${client}]
 user = ${client}
@@ -98,13 +100,10 @@ systemctl restart nginx
 systemctl restart php7.4-fpm
 
 #database
-echo "Creating mariadb database"
+echo "\nCreating mariadb database"
 
 query_mysql="CREATE DATABASE ${client};
 GRANT ALL PRIVILEGES ON ${client}.* TO '${client}'@'%' IDENTIFIED BY '${password}';
 FLUSH PRIVILEGES;"
 
-echo "${query_mysql}"
-
 mysql --execute="${query_mysql}"
-
